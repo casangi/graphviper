055685edfa1540d222f7b47ef0a5ec13a6739a86
Jan-Willem <jwsteeb@gmail.com>
2024-01-31 09:55:44 -0500

    Fix funcution link in docstring of reduce.

--- src/graphviper/graph_tools/reduce.py
+++ src/graphviper/graph_tools/reduce.py
@@ -3,12 +3,12 @@ from typing import Dict, Union
 import dask
 
 def reduce(graph: list, reduce_node_task: Callable[..., Any], input_params: Dict, mode:{"tree","single_node"}="tree")->list:
-    """Appends a reduce step to the graph created by the :func:`graphviper.graph_tools.map`_. function. 
+    """Appends a reduce step to the graph created by the :func:`graphviper.graph_tools.map`. function.
 
     Parameters
     ----------
     graph : list
-        Graph produced by :func:`graphviper.graph_tools.map`_.
+        Graph produced by :func:`graphviper.graph_tools.map`.
     reduce_node_task : _type_
        The function that forms the nodes in the reduce portion of the graph must have two parameters: ``input_data`` and ``input_params``. The ``input_data`` represents the output from the mapping nodes, while ``input_params`` comes from the ``reduce`` parameter with the same name.
     input_params : Dict
