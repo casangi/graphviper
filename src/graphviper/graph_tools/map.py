c840955cbcdf6250245178ed697ebd4a63309935
Jan-Willem <jwsteeb@gmail.com>
2024-01-31 09:47:20 -0500

    Update docs.

--- src/graphviper/graph_tools/map.py
+++ src/graphviper/graph_tools/map.py
@@ -13,25 +13,27 @@ def map(
     input_data: Union[Dict, processing_set],
     node_task_data_mapping: dict,
     node_task: Callable[..., Any],
-    input_parms: dict,
+    input_params: dict,
     in_memory_compute: bool = False,
     client=None,
     date_time: str = None,
 )->list:
-    """Builds a perfectly parallel graph where a node is created for each item in the :ref:`node_task_data_mapping <node task data mapping>`.
+    """Create a perfectly parallel graph where a node is generated for each item in the :ref:`node_task_data_mapping <node task data mapping>` using the function specified in the ``node_task`` parameter.
 
     Parameters
     ----------
     input_data : Union[Dict, processing_set]
-        Can either be a `processing_set <https://github.com/casangi/xradio/blob/main/src/xradio/vis/_processing_set.py>`_ or a Dictionary of `xarray.Datasets <https://docs.xarray.dev/en/stable/generated/xarray.Dataset.html>`_. Only coordinates are needed so no actual data is loaded into memory.
+        Can either be a `processing_set <https://github.com/casangi/xradio/blob/main/src/xradio/vis/_processing_set.py>`_ or a Dictionary of `xarray.Datasets <https://docs.xarray.dev/en/stable/generated/xarray.Dataset.html>`_. Only coordinates are needed so no actual data is loaded into memory (except if ``in_memory_compute`` is True).
     node_task_data_mapping :
         Node task data mapping dictionary. See :ref:`notes <node task data mapping>` for structure of dictionary.
     node_task : Callable[..., Any]
         The function that forms the nodes in the graph. The function must have a single input parameter that must be a dictionary. The ``input_params``, along with graph and coordinate-related parameters, will be passed to this function.
-    input_parms : Dict
-        The input parameters to be passed to node_task.
+    input_params : Dict
+        The input parameters to be passed to ``node_task``. 
+        See notes for input parameters requirements.
     in_memory_compute : optional
-        If true the lazy arrays in ``input_data`` are loaded into memory using `xarray.Dataset.load <https://docs.xarray.dev/en/stable/generated/xarray.Dataset.load.html>`_ , by default False.
+        Setting ``in_memory_compute`` can lead to memory issues since all data is loaded into memory. Consequently, this option should only be used for testing purposes. If true the lazy arrays in ``input_data`` are loaded into memory using `xarray.Dataset.load <https://docs.xarray.dev/en/stable/generated/xarray.Dataset.load.html>`_ , by default False.
+        See notes of how to access data in ``node_task``. 
     client : optional
         The Dask client is only required if local caching is enabled see :func:`graphviper.dask.client.slurm_cluster_client` , by default None.
     date_time : str, optional
@@ -41,9 +43,40 @@ def map(
     -------
     List:
         List of `dask.delayed <https://docs.dask.org/en/latest/delayed-api.html>`_ objects that represent the ``Dask`` graph.
+
+    Notes
+    -----
+    The ``input_params`` dictionary will be passed to each instance of the ``node_task`` along with the following items from the :ref:`node_task_data_mapping <node task data mapping>`:
+
+        - chunk_indices
+        - parallel_dims
+        - data_selection
+        - task_coords
+        - task_id
+
+    If ``in_memory_compute`` is ``False`` then ``input_params["input_data_store"]`` must be provided with a ``MutableMapping`` where a ``Zarr Group`` has been stored or a path to a directory in file system where a ``Zarr DirectoryStore`` has been stored.
+
+    If local caching is enabled the following will also be included with the `input_params` dictionary:
+
+        - date_time
+        - viper_local_dir
+
+    Example of how to access data in ``node_task``::
+
+        if input_params["input_data"] is None: #in_memory_compute==False
+            ps = load_data(
+                input_params["input_data_store"], data_selection=input_params["data_selection"]
+            )
+        else: #in_memory_compute==True
+            ps = input_params["input_data"]
+
+    , where ``load_data`` is appropriate function for your data.
+
     """
     n_tasks = len(node_task_data_mapping)
 
+    #Get local_cache configuration if enabled in graphviper.dask.client.slurm_cluster_client.
+    # local_cache will be True if enabled.
     (
         local_cache,
         viper_local_dir,
@@ -53,30 +86,31 @@ def map(
     ) = _local_cache_configuration(n_tasks, client, date_time)
 
     graph_list = []
+    #Create a node in Dask graph for each task_id in node_task_data_mapping
     for task_id, node_task_parameters in node_task_data_mapping.items():
         # print(task_id, node_task_parameters.keys())
-        input_parms.update(node_task_parameters)
-        input_parms["date_time"] = date_time
-        input_parms["viper_local_dir"] = viper_local_dir
-        input_parms["task_id"] = task_id
+        input_params.update(node_task_parameters)
+        input_params["task_id"] = task_id
 
-        if in_memory_compute:
-            input_parms["input_data"] = _select_data(
-                input_data, input_parms["data_selection"]
+        if in_memory_compute: #Data gets loaded into memory.
+            input_params["input_data"] = _select_data(
+                input_data, input_params["data_selection"]
             )
         else:
-            input_parms["input_data"] = None
+            input_params["input_data"] = None
 
         if local_cache:
+            input_params["date_time"] = date_time
+            input_params["viper_local_dir"] = viper_local_dir
             node_ip = nodes_ip_list[tasks_to_node_map[task_id]]
-            input_parms["node_ip"] = node_ip
+            input_params["node_ip"] = node_ip
             with dask.annotate(resources={node_ip: 1}):
-                graph_list.append(dask.delayed(node_task)(dask.delayed(input_parms)))
+                graph_list.append(dask.delayed(node_task)(dask.delayed(input_params)))
         else:
-            # print("input_parms",input_parms)
-            graph_list.append(dask.delayed(node_task)(dask.delayed(input_parms)))
+            input_params["date_time"] = None
+            graph_list.append(dask.delayed(node_task)(dask.delayed(input_params)))
 
-    return graph_list, input_parms["date_time"]
+    return graph_list, input_params["date_time"]
 
 
 def _select_data(input_data, data_selection):
--- src/graphviper/graph_tools/reduce.py
+++ src/graphviper/graph_tools/reduce.py
@@ -3,23 +3,24 @@ from typing import Dict, Union
 import dask
 
 def reduce(graph: list, reduce_node_task: Callable[..., Any], input_params: Dict, mode:{"tree","single_node"}="tree")->list:
-    """_summary_
+    """Appends a reduce step to the graph created by the :func:`graphviper.graph_tools.map`_. function. 
 
     Parameters
     ----------
     graph : list
         Graph produced by :func:`graphviper.graph_tools.map`_.
     reduce_node_task : _type_
-        _description_
+       The function that forms the nodes in the reduce portion of the graph must have two parameters: ``input_data`` and ``input_params``. The ``input_data`` represents the output from the mapping nodes, while ``input_params`` comes from the ``reduce`` parameter with the same name.
     input_params : Dict
-        _description_
-    mode : tree&quot;,&quot;single_node, optional
-        _description_, by default "tree"
+        The input parameters to be passed to ``node_task``. 
+    mode : {"tree","single_node"}, optional
+        - ``single_node``: The output from all `map` nodes is sent to a single node,
+        - ``tree``: The outputs are combined using a binary tree reduction, by default "tree"
 
     Returns
     -------
     list
-        _description_
+        List of a single `dask.delayed <https://docs.dask.org/en/latest/delayed-api.html>`_ objects that represent the ``reduce`` Dask graph.
     """
     if mode == "tree":
         graph_reduced = _tree_combine(
