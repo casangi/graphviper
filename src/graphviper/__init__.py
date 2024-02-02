1c9d0f083beb46e11682c10dfd7cd9eacd3c2128
Joshua Hoskins <jhoskins@jlab.org>
2024-02-02 09:25:05 -0700

    Add parameter environment setup to __init__.py

--- src/graphviper/__init__.py
+++ src/graphviper/__init__.py
@@ -5,6 +5,7 @@ from graphviper.utils.logger import setup_logger
 
 __version__ = version("graphviper")
 
+# Setup default logger instance for module
 if not os.getenv("VIPER_LOGGER_NAME"):
     os.environ["VIPER_LOGGER_NAME"] = "graphviper"
     setup_logger(
@@ -14,3 +15,13 @@ if not os.getenv("VIPER_LOGGER_NAME"):
         log_file="graphviper-logfile",
         log_level="INFO",
     )
+
+# Setup environment variable to identify the configuration directory for the module
+if os.path.exists(os.path.dirname(__file__) + "/config/"):
+    if not os.getenv("PARAMETER_CONFIG_PATH"):
+        os.environ["PARAMETER_CONFIG_PATH"] = os.path.dirname(__file__) + "/config/"
+
+    else:
+        if os.path.dirname(__file__) + "/config/" not in os.getenv("PARAMETER_CONFIG_PATH"):
+            os.environ["PARAMETER_CONFIG_PATH"] = "/config/"
+
