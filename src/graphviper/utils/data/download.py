665d51838e90ce79d218da88ba4b13ead91cdd1f
Joshua Hoskins <jrh4dy@gmail.com>
2024-04-30 13:48:21 -0400

    Better implementation of threaded download. This will be teh default eventually.

--- src/graphviper/utils/data/download.py
+++ src/graphviper/utils/data/download.py
@@ -1,5 +1,9 @@
+import psutil
 import pathlib
 import graphviper
+import concurrent.futures
+
+import graphviper.utils.logger as logger
 
 from typing import NoReturn
 from graphviper.utils.console import Colorize
@@ -21,7 +25,7 @@ def download(file: str, folder: str = ".", source="local") -> NoReturn:
     -------
         No return
     """
-    
+
     if not pathlib.Path(folder).resolve().exists():
         colorize = Colorize()
 
@@ -32,17 +36,49 @@ def download(file: str, folder: str = ".", source="local") -> NoReturn:
         graphviper.utils.data.remote.download(file=file, folder=folder)
 
     elif source == "threaded":
-        import concurrent.futures
 
         if not isinstance(file, list):
             file = [file]
 
-        with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
-            executor.map(
-                graphviper.utils.data.dropbox.download,
-                file
-            )
+        n_threads = get_usable_threads(len(file))
+        logger.debug(f"Initializing with {n_threads} threads.")
 
+        print_file_list(file)
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=n_threads) as executor:
+            for f in file:
+                executor.submit(
+                    graphviper.utils.data.dropbox.download,
+                    f,
+                    folder
+                )
 
     else:
         graphviper.utils.data.dropbox.download(file=file, folder=folder)
+
+
+def get_usable_threads(n_files: int) -> int:
+    import threading
+
+    available_threads = psutil.cpu_count(logical=True) - threading.active_count()
+
+    if available_threads > n_files:
+        return n_files
+
+    return int(available_threads)
+
+
+def print_file_list(files: list) -> NoReturn:
+    from rich.table import Table
+    from rich.console import Console
+    from rich import box
+
+    console = Console()
+    table = Table(show_header=True, box=box.SIMPLE)
+
+    table.add_column("Download List", justify="center")
+
+    for file in files:
+        table.add_row(f"[magenta]{file}[/magenta]")
+
+    console.print(table)
