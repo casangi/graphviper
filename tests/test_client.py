a01dd97d0e25f1fec88b929306a12a1c0f843b7d
Joshua Hoskins <jrh4dy@gmail.com>
2024-05-14 12:22:59 -0400

    Clean up comments in tests.

--- tests/test_client.py
+++ tests/test_client.py
@@ -47,19 +47,12 @@ class TestGraphViperClient:
         astrohack Dask client.
         """
 
-        #log_params = {"log_level": "DEBUG"}
-
-        #client = local_client(cores=2, memory_limit="8GB", log_params=log_params)
-
         try:
             if graphviper.dask.client._current_client.get() is None:
                 raise OSError
 
         except OSError:
             assert False
-
-        #finally:
-        #    client.shutdown()
 
     def test_client_dask_dir(self):
         """
@@ -68,15 +61,8 @@ class TestGraphViperClient:
         """
 
         try:
-            #log_params = {"log_level": "DEBUG"}
 
             path = pathlib.Path(".").cwd() / "dask_test_dir"
-            #client = local_client(
-            #    cores=2,
-            #    memory_limit="8GB",
-            #    log_params=log_params,
-            #    dask_local_dir=str(path),
-            #)
 
             if path.exists() is False:
                 raise FileNotFoundError
@@ -84,26 +70,11 @@ class TestGraphViperClient:
         except FileNotFoundError:
             assert False
 
-        #finally:
-        #    client.shutdown()
-
     def test_client_logger(self):
         """
         Run astrohack_local_client with N cores and with a memory_limit of M GB without any errors and the messages
         will be logged in the terminal.
         """
-
-        #log_params = {
-        #    "log_level": "DEBUG",
-        #    "log_to_file": True,
-        #    "log_file": "graphviper_log_file",
-        #}
-
-        #client = local_client(
-        #    cores=2,
-        #    memory_limit="8GB",
-        #    log_params=log_params
-        #)
 
         files = os.listdir(".")
 
@@ -116,6 +87,3 @@ class TestGraphViperClient:
 
         except FileNotFoundError:
             assert False
-
-        #finally:
-        #    client.shutdown()
